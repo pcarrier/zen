bcb3046fbbb12790f561106b11670e8a7f545741
Pierre Carrier <pc@rrier.ca>
2020-02-22 01:44:10 -0500

    hop

--- build.fsx
+++ build.fsx
@@ -1,21 +1,24 @@
 #r "paket:
 nuget Fake.DotNet.Cli
+nuget Fake.DotNet.Paket
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target //"
+
 #load ".fake/build.fsx/intellisense.fsx"
+
 open Fake.Core
+open Fake.Core.TargetOperators
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
-open Fake.Core.TargetOperators
-
+open Fake.Paket
 open System.IO
 
 Target.initEnvironment ()
 
 Target.create "Restore" (fun _ ->
-    Shell.Exec (Path.Combine(".paket", "paket.exe"), "restore", "") |> ignore
+    Paket.Restore
 )
 
 Target.create "Clean" (fun _ ->
