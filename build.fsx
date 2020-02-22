e441805023d45cb3f16d3bb62dc2cee2fa095eaa
Pierre Carrier <pc@rrier.ca>
2020-02-22 01:27:50 -0500

    hop

---
+++ build.fsx
@@ -0,0 +1,39 @@
+#r "paket:
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+open System.IO
+
+Target.initEnvironment ()
+
+Target.create "Restore" (fun _ ->
+    Shell.Exec (Path.Combine(".paket", "paket.exe"), "restore", "") |> ignore
+)
+
+Target.create "Clean" (fun _ ->
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    |> Shell.cleanDirs
+)
+
+Target.create "Build" (fun _ ->
+    !! "src/**/*.*proj"
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "All" ignore
+
+"Clean"
+  ==> "Restore"
+  ==> "Build"
+  ==> "All"
+
+Target.runOrDefault "All"
